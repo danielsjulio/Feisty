143a7b1d68ebb531d845c2fc0516c6267cfc03f1
Gil Julio <gilsjulio@gmail.com>
2015-05-10 23:34:42 +0100

    Main heading

--- app/src/main/java/com/feisty/MainActivity.java
+++ app/src/main/java/com/feisty/MainActivity.java
@@ -1,19 +1,93 @@
 package com.feisty;
 
+import android.app.Activity;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import java.util.Locale;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
+    @InjectView(R.id.pager)
+    ViewPager mViewPager;
+
+    @InjectView(R.id.toolbar)
+    Toolbar mToolbarView;
+
+    @InjectView(R.id.sliding_tabs)
+    SlidingTabLayout mSlidingTabLayout;
+
+    @InjectView(R.id.header)
+    RelativeLayout mHeader;
+
+    @InjectView(R.id.flexible_space)
+    View mFlexibleSpaceView;
+
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    int mFlexibleSpaceHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        setSupportActionBar(mToolbarView);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+
+        mSlidingTabLayout.setCustomTabView(R.layout.tab_indicator, android.R.id.text1);
+
+        Resources res = getResources();
+        mSlidingTabLayout.setSelectedIndicatorColors(res.getColor(R.color.tab_selected_strip));
+        mSlidingTabLayout.setDistributeEvenly(true);
+        mSlidingTabLayout.setViewPager(mViewPager);
+
+        mFlexibleSpaceHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_height);
+
+        int flexibleSpaceAndToolbarHeight = mFlexibleSpaceHeight + getActionBarSize();
+        mFlexibleSpaceView.getLayoutParams().height = flexibleSpaceAndToolbarHeight;
+        mFlexibleSpaceView.setLayoutParams(mFlexibleSpaceView.getLayoutParams());
+
+
+//        findViewById(R.id.body).setPadding(0, flexibleSpaceAndToolbarHeight, 0, 0);
+
     }
 
+    protected int getActionBarSize() {
+        TypedValue typedValue = new TypedValue();
+        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
+        int indexOfAttrTextSize = 0;
+        TypedArray a = this.obtainStyledAttributes(typedValue.data, textSizeAttr);
+        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
+        a.recycle();
+        return actionBarSize;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +109,40 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                case 1:
+                    return VideoListFragment.newInstance();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return "videos".toUpperCase(l);
+                case 1:
+                    return "series".toLowerCase(l);
+            }
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/feisty/VideoListFragment.java
@@ -0,0 +1,48 @@
+package com.feisty;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link VideoListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class VideoListFragment extends Fragment {
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment VideoListFragment.
+     */
+    public static VideoListFragment newInstance() {
+        VideoListFragment fragment = new VideoListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public VideoListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_video_list, container, false);
+    }
+
+
+}
